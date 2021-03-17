8431c38dbea7615068314288ae35055996959818
Jack Curtin <jackcurtin94@gmail.com>
2021-03-17 14:45:58 -0500

    each player can now set their name once

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const allBoxes = document.querySelectorAll('.box');
 const gameText = document.querySelector('h2');
 const clearBtn = document.querySelector('#clearBtn');
+const nameForms = document.querySelectorAll('.name-input');
 const winningCombos = [
     ['box1', 'box2', 'box3'],
     ['box1', 'box4', 'box7'],
@@ -13,13 +14,26 @@ const winningCombos = [
 ];
 
 class Player {
-    constructor(name, color) {
+    constructor(name, color,playerIndex) {
         this.name = name
+        this.nameChanged = false;
         this.color = color
         this.playerChoices = []
         this.playerWinningCombo = []
         this.playerTotalVictories = 0
+        this.playerIndex = playerIndex
     }
+
+    changeName(){
+        if (this.nameChanged === false) {
+            this.name = nameForms[this.playerIndex].querySelector('input').value;
+            this.nameChanged = true;
+            Game.clearGame();
+        } else {
+            alert ('you cannot change your name again')
+        }
+    }
+
     claimBox(currentBox){
         document.querySelector(`#${currentBox}`).style.backgroundColor = `${this.color}`;
         this.playerChoices.push(currentBox);
@@ -35,6 +49,9 @@ class Player {
                         this.playerWinningCombo = winningCombos[i];
                         Game.winner = `${this.name}`;
                         this.playerTotalVictories++;
+                        this.playerWinningCombo.forEach(box => {
+                            document.querySelector(`#${box}`).style.border = '20px solid gold';
+                        })
                     }
                 }
             }
@@ -42,8 +59,8 @@ class Player {
     }
 }
 
-let playerOne = new Player('me', 'red');
-let playerTwo = new Player('you','blue');
+let playerOne = new Player('Player 1', 'red', 0);
+let playerTwo = new Player('Player 2','blue', 1);
 
 const Game = {
     players: [playerOne, playerTwo],
@@ -62,9 +79,11 @@ const Game = {
     clearGame(){
         allBoxes.forEach(box => {
             box.style.backgroundColor = null;
+            box.style.border = '1px solid black';
         })
         this.winner = null;
         this.occupiedSquares = [];
+        this.turn = playerOne.name;
         playerOne.playerChoices = [];
         playerTwo.playerChoices = [];
         gameText.innerHTML = `${Game.turn} click a box to start!`
@@ -75,7 +94,9 @@ allBoxes.forEach(box =>{
     box.addEventListener('click', (e) => {
         let currentBox = e.target.id;
         if (!Game.occupiedSquares.includes(currentBox)){
-            if (Game.turn === playerOne.name){
+            if (Game.turn === null){
+                Game.turn = playerOne.name;
+            }else if (Game.turn === playerOne.name){
                 playerOne.claimBox(currentBox);
                 Game.turn = playerTwo.name;
             } else{
@@ -90,10 +111,23 @@ allBoxes.forEach(box =>{
     })
 })
 
+nameForms.forEach(form => {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if (form.id === 'player1Form') {
+            playerOne.changeName();
+        } else {
+            playerTwo.changeName();
+        }
+        form.querySelector('input').value = null;
+    })
+})
+
 clearBtn.addEventListener('click', () => {
     Game.clearGame();
 })
-gameText.innerHTML = `${Game.turn} click a box to start!`
+
+gameText.innerHTML = `${Game.turn} click a box to start!`;
 
 
 
