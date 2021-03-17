6af03ac3ca99874aa0d2498e5420501c8c7b9716
Jack Curtin <jackcurtin94@gmail.com>
2021-03-17 12:30:29 -0500

    added clear button and winning combo to player class

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 const allBoxes = document.querySelectorAll('.box');
+const gameText = document.querySelector('h2');
+const clearBtn = document.querySelector('#clearBtn');
 const winningCombos = [
     ['box1', 'box2', 'box3'],
     ['box1', 'box4', 'box7'],
@@ -15,6 +17,7 @@ class Player {
         this.name = name
         this.color = color
         this.playerChoices = []
+        this.playerWinningCombo = []
         this.playerTotalVictories = 0
     }
     claimBox(currentBox){
@@ -29,8 +32,9 @@ class Player {
             if (this.playerChoices.indexOf(winningCombos[i][0]) >= 0){
                 if (this.playerChoices.indexOf(winningCombos[i][1]) >= 0){
                     if (this.playerChoices.indexOf(winningCombos[i][2]) >= 0){
-                        Game.winner = `${this.name}`
-                        this.playerTotalVictories++
+                        this.playerWinningCombo = winningCombos[i];
+                        Game.winner = `${this.name}`;
+                        this.playerTotalVictories++;
                     }
                 }
             }
@@ -50,10 +54,9 @@ const Game = {
     checkWinner() {
         if (this.winner === playerOne.name || this.winner === playerTwo.name) {
             console.log(`${this.winner}  wins!`)
-            this.clearGame()
+            gameText.innerText= `${this.winner}  wins!`;
         } else if (this.occupiedSquares.length === 9 && this.winner === null){
             console.log('it is a draw');
-            Game.clearGame();
         }
     },
     clearGame(){
@@ -64,6 +67,7 @@ const Game = {
         this.occupiedSquares = [];
         playerOne.playerChoices = [];
         playerTwo.playerChoices = [];
+        gameText.innerHTML = `${Game.turn} click a box to start!`
     }
 }
 
@@ -78,10 +82,18 @@ allBoxes.forEach(box =>{
                 playerTwo.claimBox(currentBox);
                 Game.turn = playerOne.name;
             }
+            gameText.innerHTML = `It is ${Game.turn}\'s turn!`
             Game.checkWinner();
         } else{
             console.log('try again')
         }
     })
 })
+
+clearBtn.addEventListener('click', () => {
+    Game.clearGame();
+})
+gameText.innerHTML = `${Game.turn} click a box to start!`
+
+
 
